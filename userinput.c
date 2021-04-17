a2e61a7bb9d4663b975c0e7bf5bdff5d13536174
khairulimran-99 <khairul991018gmail.com>
2021-04-17 15:18:21 +0000

    Exercise Coding

---
+++ userinput.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void)
+{
+	/*This is my secodn program in C*/
+	int age;
+	printf("Hi, how old are you?>");
+	scanf("%d", &age);
+	printf("You are %d years old", age);
+	return(0);
+}
+
+
